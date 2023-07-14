10215e7fe1d655687deb17a4ead4b16cee6b0743
trieuphan0211 <trieu.207ct28659@vanlanguni.vn>
2023-07-14 10:48:16 +0700

    first commit

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: "cluster0.c7srckc.mongodb.net",
+  DB_USER: "trieu0211",
+  DB_PASS: "trieu0211",
+  DB_DATABASE: "shoppingOnline",
+  EMAIL_USER: "trieu12080211@gmail.com", // Microsoft mail service
+  EMAIL_PASS: "fbeckgcugtvafcsl",
+  JWT_SECRET: "ngoctrieu0211",
+  JWT_EXPIRES: "3600000", // in milliseconds
+};
+module.exports = MyConstants;
